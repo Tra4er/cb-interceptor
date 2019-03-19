ebd653baff2601c0e93dad24b7a113a8401ec1ff
Illia Trach <itrach@datas-tech.com>
2019-03-19 18:26:41 +0200

    Empty project and readme.

---
+++ src/main/java/com/itrach/cbinterceptor/CbInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.itrach.cbinterceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbInterceptorApplication.class, args);
+	}
+
+}
